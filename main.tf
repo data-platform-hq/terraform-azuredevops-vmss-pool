5c293f2d96bca6e5d25c88508464e0b94a5add6b
Leonid Frolov <51460118+leonidfrolov@users.noreply.github.com>
2024-01-09 10:29:51 +0200

    fix: added resource index

--- main.tf
+++ main.tf
@@ -22,15 +22,15 @@ data "azuredevops_project" "this" {
 
 data "azuredevops_serviceendpoint_azurerm" "this" {
   count = var.create_ado_resources ? 1 : 0
-  project_id            = data.azuredevops_project.this.id
+  project_id            = data.azuredevops_project.this[0].id
   service_endpoint_name = var.ado_service_connection_azurerm_name
 }
 
 resource "azuredevops_elastic_pool" "this" {
   count = var.create_ado_resources ? 1 : 0
   name                   = var.ado_vmss_pool_name
-  service_endpoint_id    = data.azuredevops_serviceendpoint_azurerm.this.id
-  service_endpoint_scope = data.azuredevops_project.this.id
+  service_endpoint_id    = data.azuredevops_serviceendpoint_azurerm.this[0].id
+  service_endpoint_scope = data.azuredevops_project.this[0].id
   azure_resource_id      = module.vmss.id
   desired_idle           = var.ado_vmss_pool_configuration.desired_idle
   max_capacity           = var.ado_vmss_pool_configuration.max_capacity
@@ -40,13 +40,13 @@ resource "azuredevops_elastic_pool" "this" {
 
 resource "azuredevops_agent_queue" "this" {
   count = var.create_ado_resources ? 1 : 0
-  project_id    = data.azuredevops_project.this.id
-  agent_pool_id = azuredevops_elastic_pool.this.id
+  project_id    = data.azuredevops_project.this[0].id
+  agent_pool_id = azuredevops_elastic_pool.this[0].id
 }
 
 resource "azuredevops_pipeline_authorization" "this" {
   count = var.create_ado_resources ? 1 : 0
-  project_id  = data.azuredevops_project.this.id
-  resource_id = azuredevops_agent_queue.this.id
+  project_id  = data.azuredevops_project.this[0].id
+  resource_id = azuredevops_agent_queue.this[0].id
   type        = "queue"
 }
