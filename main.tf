d0891cf1c86a27bc5cb42a80305aeb43fe60bc71
Leonid Frolov <51460118+leonidfrolov@users.noreply.github.com>
2024-01-09 10:31:07 +0200

    fix: fmt

--- main.tf
+++ main.tf
@@ -17,17 +17,17 @@ module "vmss" {
 
 data "azuredevops_project" "this" {
   count = var.create_ado_resources ? 1 : 0
-  name = var.ado_project_name
+  name  = var.ado_project_name
 }
 
 data "azuredevops_serviceendpoint_azurerm" "this" {
-  count = var.create_ado_resources ? 1 : 0
+  count                 = var.create_ado_resources ? 1 : 0
   project_id            = data.azuredevops_project.this[0].id
   service_endpoint_name = var.ado_service_connection_azurerm_name
 }
 
 resource "azuredevops_elastic_pool" "this" {
-  count = var.create_ado_resources ? 1 : 0
+  count                  = var.create_ado_resources ? 1 : 0
   name                   = var.ado_vmss_pool_name
   service_endpoint_id    = data.azuredevops_serviceendpoint_azurerm.this[0].id
   service_endpoint_scope = data.azuredevops_project.this[0].id
@@ -39,13 +39,13 @@ resource "azuredevops_elastic_pool" "this" {
 }
 
 resource "azuredevops_agent_queue" "this" {
-  count = var.create_ado_resources ? 1 : 0
+  count         = var.create_ado_resources ? 1 : 0
   project_id    = data.azuredevops_project.this[0].id
   agent_pool_id = azuredevops_elastic_pool.this[0].id
 }
 
 resource "azuredevops_pipeline_authorization" "this" {
-  count = var.create_ado_resources ? 1 : 0
+  count       = var.create_ado_resources ? 1 : 0
   project_id  = data.azuredevops_project.this[0].id
   resource_id = azuredevops_agent_queue.this[0].id
   type        = "queue"
