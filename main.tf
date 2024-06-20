99bbe0178da178bcacc1cc0aa882953d163ce813
Oleh Mykolaishyn <owlleg6@gmail.com>
2024-06-20 14:59:19 +0300

    fix: update vmss version to 1.2.6

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "this" {
 
 module "vmss" {
   source  = "data-platform-hq/vmss/azurerm"
-  version = "1.2.5"
+  version = "1.2.6"
 
   scale_set_name              = var.vm_scale_set_name
   location                    = var.location
